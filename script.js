d04b5fe27b0fc3a69e4440df5dc33a1790f07ce0
Het Patel <hpatel9731@conestogac.on.ca>
2025-02-23 23:54:01 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    const shopNowButton = document.getElementById("shopNowBtn");
+    
+    shopNowButton.addEventListener("click", function () {
+        alert("Redirecting to Shop...");
+    });
+});
